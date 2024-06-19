e03ff6c20b367d676456c99a214b6d32d2b476a6
Eve <eve_braun@mail.ru>
2024-06-19 21:28:38 +0300

    2

--- get_weather/main.py
+++ get_weather/main.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from bs4 import BeautifulSoup as bs
 from fake_useragent import UserAgent
 
@@ -15,30 +16,54 @@ def get_html(url):
 def get_weather(html:str):
     # создаем объект html-страницы в формате bs
     soup = bs(html, 'html.parser')
-    date = soup.find('div', class_="dates short-d").text.split(', ') 
+    date = soup.find('div', class_="dates short-d") 
     # <div class="dates short-d"><span>Среда</span>, 19 июня</div>
+    if date:
+        date = soup.find('div', class_="dates short-d").text.split(', ')
+    else:
+        date = ['None', 'None']
+
     day_of_week =date[0]
     day_and_month =date[1]
 
-    table = soup.find('div', class_='weather-short')
+    table = soup.find('table', class_='weather-today short')
     rows = table.find_all('tr')
+    weather_today = {"day_of_week": day_of_week, 
+                     'day_and_month': day_and_month}
 
-    # <div class="weather-short"><div class="dates short-d"><span>Четверг</span>, 20 июня</div><div class="pane"></div><table class="weather-today short"></table><table class="weather-today"><tbody><tr class="night"><td class="weather-day">Ночь</td><td class="weather-temperature"><div class="wi n320 wi-v tooltip" title="Местами сильный дождь"></div><span>+14°</span></td><td class="weather-feeling">+14°</td><td class="weather-probability">77%</td><td class="weather-pressure">750</td><td class="weather-wind"><span class="tooltip wwi W" title="западный"></span> <span title=" 7.6 км/ч" class="tooltip ">2.1</span></td><td class="weather-humidity">90%</td></tr><tr class="morning"><td class="weather-day">Утро</td><td class="weather-temperature"><div class="wi d300 wi-v tooltip" title="Преимущественно облачно"></div><span>+15°</span></td><td class="weather-feeling">+15°</td><td class="weather-probability">41%</td><td class="weather-pressure">751</td><td class="weather-wind"><span class="tooltip wwi W" title="западный"></span> <span title=" 11.2 км/ч" class="tooltip ">3.1</span></td><td class="weather-humidity">75%</td></tr><tr class="day"><td class="weather-day">День</td><td class="weather-temperature"><div class="wi d100 wi-v tooltip" title="Незначительная облачность"></div><span>+18°</span></td><td class="weather-feeling">+18°</td><td class="weather-probability">39%</td><td class="weather-pressure">754</td><td class="weather-wind"><span class="tooltip wwi W" title="западный"></span> <span title=" 15.5 км/ч" class="tooltip ">4.3</span></td><td class="weather-humidity">59%</td></tr><tr class="evening"><td class="weather-day">Вечер</td><td class="weather-temperature"><div class="wi n000 wi-v tooltip" title="Ясно"></div><span>+17°</span></td><td class="weather-feeling">+17°</td><td class="weather-probability">1%</td><td class="weather-pressure">755</td><td class="weather-wind"><span class="tooltip wwi W" title="западный"></span> <span title=" 11.9 км/ч" class="tooltip ">3.3</span></td><td class="weather-humidity">
-    # print(date)
+   
+    for row in rows:
+        weather_day = row.find('td', class_="weather-day").text
+        temperature = row.find('td', class_="weather-temperature").text
+        # atmosfera = weather_day.find('div').get('title')
+        weather_feeling = row.find('td', class_='weather-feeling').text
+        weather_pressure = row.find('td', class_='weather-pressure').text
+        
+        wind_direction = row.find('td', class_='weather-wind').find_all('span')[0].get('title')
+        wind_speed_ms =row.find('td', class_='weather-wind').find_all('span')[1].text
+        humidity = row.find('td', class_='weather-humidity').text
 
-    row = rows[0]
-    time_of_day = row.find('td', class_="weather-day").text
-    temperature = row.find('td', class_="weather-temperature").text
-    atmosfera = time_of_day.find('div').get('title')
-    weather_feeling = row.find('td', class_='weather-feeling').text
-    weather_pressure = row.find('td', class_='weather-pressure').text
-    wind_direction_kmch = row.find('td', class_='weather-wind').find_all('span')[0].get('title')
-    wind_direction_ms =row.find('td', class_='weather-wind').find_all('span')[1].text
-    humidity = row.find('td', class_='weather-humidity').text
+        weather_today[weather_day] = {
+            'temperature': temperature,
+            # 'atmosfera': atmosfera,
+            'weather_feeling': weather_feeling,
+            'weather_pressure': weather_pressure,
+            'wind': {
+                'wind_direction': wind_direction,
+                'wind_speed_ms': wind_speed_ms,
+            },
+            'humidity': humidity
+        }
+    return weather_today
 
 URL = 'https://world-weather.ru/pogoda/russia/saint_petersburg/'
 # PATH-params
 
 
 html = get_html(URL)
-get_weather(html)
+weather_info = get_weather(html)
+weather_json = json.dumps(weather_info, ensure_ascii=False)
+print(weather_json)
+
+
+# что-то тут не работает
